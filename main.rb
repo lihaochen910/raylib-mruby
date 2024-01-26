c2e9e946bafe701988240c6437026dcc38d4f347
Kanbaru <lihaochen910@hotmail.com>
2024-01-27 00:48:57 +0800

    new generated code for test.

--- main.rb
+++ main.rb
@@ -1,12 +1,19 @@
+puts Raylib::VERSION
 
 Raylib::init_window 800, 600, "Window"
 Raylib::set_target_fps 60
+
+bg_color = Raylib::Color.new
+bg_color.r = 1
+bg_color.g = 0
+bg_color.b = 0
+bg_color.a = 1
 
 until Raylib::window_should_close
 
     Raylib::begin_drawing
 
-    Raylib::clear_background 1,0,0,1
+    Raylib::clear_background bg_color
     Raylib::draw_text( "Congrats! You created your first window!", 190, 200, 20, 1,1,1,1 )
 
     Raylib::end_drawing
