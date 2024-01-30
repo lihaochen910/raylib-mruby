85189c25d1ab3af2494cc6c7534417ca8923827a
Kanbaru <lihaochen910@hotmail.com>
2024-01-30 10:04:26 +0800

    add project CodeGenerator.

---
+++ projects/CodeGenerator/CodeGenerator.CLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Pixpil.RaylibMRubyBindingsGenerator;
+
+class Program {
+
+	static void Main( string[] args ) {
+
+		if ( args.Length < 2 ) {
+			Console.WriteLine( "invalid args." );
+			Console.WriteLine( "arg0: RaylibIncludePath" );
+			Console.WriteLine( "arg1: Output c file path" );
+			return;
+		}
+		
+		var raylibIncludePath = args[ 0 ];
+		var outputPath = args[ 1 ];
+
+		Console.WriteLine( $"start parsing: {raylibIncludePath}" );
+
+		string parsed = Generator.Parse( raylibIncludePath );
+		File.WriteAllText( outputPath, parsed );
+
+		Console.WriteLine( "finished." );
+
+	}
+}
